db67f9b7b95375f866a1fb3a8d31e6c311e76551
Izaac Tay <izaac@g.ucla.edu>
2023-11-07 12:31:58 -0800

    emoved header and changed date

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 13, 2023 20:00:00").getTime();
+var countDownDate = new Date("Dec 14, 2023 20:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
