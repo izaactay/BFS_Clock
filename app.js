22bbd517fcbf1c25c4885d719fa1d16d8dd92d80
Izaac Tay <izaac@g.ucla.edu>
2023-12-15 16:22:12 -0500

    added reading from env variables

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var countDownDate = new Date("Dec 14, 2023 20:00:00").getTime();
+
+//var countDownDate = new Date("Dec 14, 2023 20:00:00").getTime();
+import DATE from "./config.js";
+var countDownDate = new Date(DATE).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
